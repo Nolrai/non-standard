8e5e1cba17f3a3992b30a87fc52d9c4ebf7a406d
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-04-24 16:40:23 -0700

    Done with basic

--- src/riddle.lean
+++ src/riddle.lean
@@ -38,6 +38,21 @@ inductive day_of_week
 
 open day_of_week
 
+def day_of_week.from_sunday : day_of_week → fin 6
+  | Sunday    := 0
+  | Monday    := 1
+  | Tuesday   := 2
+  | Wednesday := 3
+  | Thursday  := 4
+  | Friday    := 5
+  | Saturday  := 6
+
+lemma from_sunday_bijection (x y : day_of_week) : 
+  x.from_sunday = y.from_sunday ↔ x = y :=
+by {
+  cases x; cases y; simp; apply_instance
+}
+
 instance : fintype day_of_week := 
   { 
     elems := 
@@ -206,45 +221,150 @@ by {
   apply bool.tt_of_inf_left,
 }
 
+lemma finite_additivity_aux (b₀ b₁ : bool) : b₀ ⊔ b₁ = tt ↔ b₀ = tt ∨ b₁ = tt :=
+by {
+  split; intros h,
+  {
+    have : tt ≤ b₀ ∨ tt ≤ b₁,
+    {
+      apply le_sup_iff.mp,
+      apply le_of_eq,
+      symmetry,
+      exact h,
+      apply_instance,
+    },
+    {
+      have H : ∀ b, tt ≤ b ↔ tt = b := λ b, by {cases b, simp, simp},
+      simp_rw eq_comm,
+      simp_rw ← H,
+      exact this,
+    },
+  },
+  {
+    cases h; rw h; simp,
+  }
+}
+
 lemma finite_additivity (α : Type) (s t : α → bool) (situation : multiset α) (disjoint : s ⊓ t = ⊥) :
   prob (s ⊔ t) situation = prob s situation + prob t situation :=
 by {
   simp_rw prob_def,
-  set d : ℤ := ↑(multiset.card situation),
-  rw rat.add_def,
-  ring_nf,
-  ring_nf,
-  rw pow_two d,
-  rw rat.mk_simplify,
-  congr,
-  rw ← multiset.card_add,
-  rw multiset.filter_add_filter,
-  have : multiset.filter (λ (a : α), s a ∧ t a) situation = 0,
-  {
-    have : (λ (a : α), s a ∧ t a) = ⊥,
-    transitivity λ x, ↥((s ⊓ t) x),
+  set d : ℤ := ↑(multiset.card situation) with d_def,
+  have d_zero : decidable (d = 0) := by apply_instance,
+  cases d_zero with d_ne_0 d_eq_0,
+  case is_false {
+    rw rat.add_def,
+    ring_nf,
+    ring_nf,
+    rw pow_two d,
+    rw rat.mk_simplify,
+    congr,
+    rw ← multiset.card_add,
+    rw multiset.filter_add_filter,
+    have : multiset.filter (λ (a : α), (↑s : α → Prop) a ∧ (↑t : α → Prop) a) situation = 0,
     {
-      unfold_coes,
-      funext, simp only [inf_apply, eq_iff_iff],
-      split; intros h,
-      { 
-        obtain ⟨h_s, h_t⟩ := h,
-        rw h_s, rw h_t,
-        rw inf_idem,
+      have : ∀ (a : α), ((↑s : α → Prop) a ∧ (↑t : α → Prop) a) = false,
+      {
+        intros a,
+        transitivity ↥((s ⊓ t) a),
+        {
+          unfold_coes,
+          funext, simp only [inf_apply, eq_iff_iff],
+          split; intros h,
+          { 
+            obtain ⟨h_s, h_t⟩ := h,
+            rw h_s, rw h_t,
+            rw inf_idem,
+          },
+          {
+            split,
+            apply bool.tt_of_inf_left _ _ h,
+            apply bool.tt_of_inf_right _ _ h,
+          },
+        },
+        {
+          rw disjoint,
+          funext, simp only [bot_eq_ff, false_iff, bot_apply, eq_iff_iff, coe_sort_ff],
+          apply not_false,
+        },
       },
       {
-        split,
-        apply bool.tt_of_inf_left _ _ h,
-        apply bool.tt_of_inf_right _ _ h,
-      },
+        simp_rw this,
+        rw multiset.filter_eq_nil,
+        intros _ _,
+        apply not_false,
+      }
     },
     {
-      rw disjoint,
-      funext, simp only [bot_eq_ff, false_iff, bot_apply, eq_iff_iff, coe_sort_ff],
-      unfold_projs,
-      rw [not_false_iff],
-      trivial
+      rw this,
+      rw add_zero,
+      congr,
+      funext,
+      unfold_coes,
+      simp,
+      apply finite_additivity_aux,
     },
-    
+    all_goals {exact d_ne_0},
+  },
+  case is_true {
+    rw d_eq_0,
+    simp_rw rat.mk_zero,
+    symmetry, apply zero_add,
   }
 }
+
+structure child := 
+  (sex : sex)
+  (born_on : day_of_week)
+
+instance : decidable_eq sex 
+  | boy   boy   := is_true rfl
+  | girl  girl  := is_true rfl
+  | boy   girl  := is_false (by {simp})
+  | girl  boy   := is_false (by {simp})
+
+instance : decidable_eq day_of_week :=
+  by {
+    intros x y,
+    cases x; cases y; simp; apply_instance
+  }
+
+instance : decidable_eq child :=
+by {
+  intros x y,
+  cases x; cases y; simp; apply_instance
+}
+
+instance : fintype child :=
+by {
+  apply fintype.of_surjective (function.uncurry child.mk),
+  intros x,
+  use (x.1, x.2),
+  cases x,
+  simp,
+}
+
+abbreviation outcome := fin 2 → child
+
+def background : multiset outcome := finset.univ.val
+
+open child
+
+def at_least_one_boy (o : outcome) := to_bool (boy ∈ ([o 1, o 2].map sex))
+
+def riddle0 : ℚ := 
+  prob at_least_one_boy background
+
+def both_boys (o : outcome) := to_bool (([o 1, o 2].map sex) = [boy, boy])
+
+def riddle1 : ℚ := 
+  prob_given both_boys at_least_one_boy background
+
+def at_least_one_boy_born_on_a_tuesday (o : outcome) := to_bool (child.mk boy Tuesday ∈ [o 1, o 2])
+
+def riddle2 : ℚ :=
+  prob_given both_boys at_least_one_boy_born_on_a_tuesday background
+
+#eval riddle0
+#eval riddle1
+#eval riddle2
